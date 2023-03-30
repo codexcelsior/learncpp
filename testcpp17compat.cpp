abdf42e980aad596957137d6973f928596db5bb8
Codexcelsior <quang13012005@gmail.com>
2023-03-30 18:01:39 +0700

    adding c_cpp_properties for the  configuration of VSCode

--- testcpp17compat.cpp
+++ testcpp17compat.cpp
@@ -8,8 +8,9 @@
 // for example, std::string and my_library::string are different things
 // but instead of having just "string"
 // you can use the prefix to select one or the other 
-namespace a::b::c {
-  inline constexpr std::string_view str("hello"); // idk what this is you're on your own for this one
+namespace a::b::c 
+{
+  inline constexpr std::string_view str{ "hello" }; // idk what this is you're on your own for this one
 }
 
 template <class... T> // a generic something
@@ -17,16 +18,14 @@ template <class... T> // a generic something
 // also google what variadics are
 std::tuple<std::size_t, std::common_type_t<T ...>>
 sum(T... args) { // a function that takes an arbitrary number of arguments
-  return {sizeof...(T), (args + ...)); // returns a tuple containing 1. how many items are passed in as variadics, and 2. the sum of those arguments
+  return {sizeof...(T), (args + ...)}; // returns a tuple containing 1. how many items are passed in as variadics, and 2. the sum of those arguments
 }
 
 int main() { 
-  auto [iNumbers, iSum] = {sum(1, 2, 3)}; // structured bindings from c++17
+  auto [iNumbers, iSum] {sum(1, 2, 3)}; // structured bindings from c++17
 
   // I'll split the prints up
-  std::cout << a::b::c::str << ' '; // prints the str variable from the a::b::c namespace
-  std::cout << iNumbers << ' ' << iSum << std::endl; // prints iNumbers, a space, iSum and a newline (std::endl flushes stdout unlike '\n' which may or may not be desirable)
-  
+  std::cout << a::b::c::str << iNumbers << ' ' << iSum << std::endl; // prints iNumbers, a space, iSum and a newline (std::endl flushes stdout unlike '\n' which may or may not be desirable)
   std::array arr{1, 2, 3}; // create a new array with an initializer list constructor
   std::cout << std::size(arr) << std::endl; // prints the size of arr, you can also do arr.size() I think
 
--- testcpp20compat.cpp
+++ testcpp20compat.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <span>
 
-struct Foo 
+struct Foo
 {
-    int a{};
-    int b{};
-    int c{};
+    int a{ };
+    int b{ };
+    int c{ };
 };
 
-consteval int sum(std::span<const int> a) //std::span and consteval 
+consteval int sum(std::span<const int>    a)
 {
     int s{ 0 };
     for (auto n : a)
@@ -17,16 +17,17 @@ consteval int sum(std::span<const int> a) //std::span and consteval
     return s;
 }
 
-auto sum(auto x, auto y) -> decltype (x + y)
+auto sum(auto x, auto y) -> decltype(x + y)
 {
     return x + y;
 }
 
 int main() 
 {
-    constexpr std::array a{3 , 2, 1};
+    constexpr std::array a{ 3, 2, 1 };
     constexpr int s{ sum(a) };
-    std::cout << sum(f1.a, f1.c) << '\n';
-    
+    Foo f1( .a = 1, .c = 3);
+    std::cout << sum(f1.a, f1.c) << std::endl;
     return 0;
 }
+
