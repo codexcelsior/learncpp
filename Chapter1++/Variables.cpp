44733b285cfb7bb4aa3db7ca7b50427df5fdabac
Codexcelsior <quang13012005@gmail.com>
2023-03-30 21:36:36 +0700

    update variables

---
+++ Chapter1++/Variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // solo variable (a is an identifier that points to the address 
+    //and is assigned a value to make the address store that value)
+    int a; //This statement calls an address a
+    int b;
+
+    //Multiple variables
+    int a,b;
+}
+
