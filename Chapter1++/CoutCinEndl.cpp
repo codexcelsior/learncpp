4acdd461dbc26360b3c915c0378bf7f49e191753
Codexcelsior <quang13012005@gmail.com>
2023-03-31 21:17:46 +0700

    Added cout vs \n

---
+++ Chapter1++/CoutCinEndl.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() 
+{
+    //cout stands for character output
+    std::cout << "Hello World!";
+    
+    //printing 4 in console
+    std::cout << 4;
+
+    //define x then assign x for printing in console
+    int x {5};
+    std::cout << x;
+
+    std::cout << "Hello" << " World!";
+
+    //Printing out both a string and an int
+    std::cout << "x is equal to: " << x;
+
+    //Endl
+    // adding std::endl will jump to new line
+    std::cout << "x is equal to: " << 10 << std::endl;
+
+    //std::endl vs '\n'
+
+    std::cout << "x is equal to: " << x << '\n';
+    std::cout << "And that's all, folks!\n";
+
+
+    return 0;
+}
--- Chapter1++/Variables.cpp
+++ Chapter1++/Variables.cpp
@@ -8,5 +8,33 @@ int main() {
 
     //Multiple variables
     int a,b;
+
+    //**VARIABLE ASSIGNMENT AND INITIALIZATION 
+
+    //VARIABLE ASSIGNMENT
+
+    int width; //define an integer variable named with 
+    width = 5; //copy assignment of value 5 into into variable width 
+    // now variable width contains 5
+    width = 7; //update the value of width from 5 to 7
+
+
+    //INITIALIZATION
+
+    //WTF is this??? -> Assignment requires 2 statements so this initialization (or init) just skips and merges 2 things together at once
+
+    int a; //no initialization(default init)
+    int b = 5; //initialization after assignment / equal sign
+    int c( 6 ); //direct initialization
+
+    int d { 7 }; //direct list init
+    int e = { 8 }; //copy list init
+    int f {}; //value init
+
+
+
+    return 0;
+
+
 }
 
