fb58a90ddc5c8d4be32b6a675663b6bb93abdd87
codexcelsior <quang13012005@gmail.com>
2023-04-05 22:38:08 +0700

    Void Done

---
+++ Chapter4++/Void.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// void value; // won't work, variables can't be defined with a void type
+
+void writeValue(int x) // void here means no return value
+{
+    std::cout << "The value of x is: " << x << '\n';
+    // no return statement, because this function doesn't return a value
+}
+
+void noReturn(int x) // void here means no return value
+{
+    // return 5; // error
+}
+
+int getValue(void)
+{
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+int getValue1()
+{
+    int x{};
+    std::cin >> x;
+    return x;
+}
