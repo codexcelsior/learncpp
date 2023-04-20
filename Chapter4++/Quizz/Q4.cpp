241dfffe77a6dbf47f0120199e17bc14a85a4f11
codexcelsior <quang13012005@gmail.com>
2023-04-20 20:06:35 +0700

    Q4 Done

---
+++ Chapter4++/Quizz/Q4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+int sec()
+{
+    int x{};
+    std::cout << "Enter the time of  the ball after x seconds falling:";
+    std::cin >> x;
+    return x;
+}
+
+int height()
+{
+    int height{};
+    std::cout << "Enter the height of the tower in meters: ";
+    std::cin >> height;
+    return height;
+}
+
+void afterFall(int height , int Sec)
+{
+
+    for (int t = 0; t < Sec ; t++)
+        std::cout << "At " << t << " seconds, the ball is at height: " << height - (t * t * 9.8 / 2) << "   meters\n";
+    std::cout << "At " << Sec << " seconds, the ball is on the ground\n";
+
+}
+
+
+int main()
+{
+
+    int Height = height();
+    int Sec = sec();
+    afterFall(Height, Sec);
+}
