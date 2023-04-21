df92e832c73a1f0fb2024d65fea15eae73d82bc2
codexcelsior <quang13012005@gmail.com>
2023-04-22 05:33:59 +0700

    new changes

--- Chapter5++/RemainAndExp.cpp
+++ Chapter5++/RemainAndExp.cpp
@@ -22,39 +22,55 @@ std::int64_t powint(std::int64_t base, int exp)
 
 
 
-
-
-
 bool isOdd(int x)
 {
     return (x % 2) == 1; // fails when x is -5
 }
 
+bool isEven(int x)
+{
+    return (x % 2) == 0;
+}
 
 
 
 int main()
 {
+    // std::cout << "Enter an integer: ";
+	// int x{};
+	// std::cin >> x;
+
+	// std::cout << "Enter another integer: ";
+	// int y{};
+	// std::cin >> y;
+
+    // std::cout << "The remainder is " << x % y << "\n";
+
+    // if ((x % y) == 0)
+    //     std::cout << x << " is evenly divisible by " << y << "\n";
+    // else
+    //     std::cout << x << " is not evenly divisible by " << y << "\n";
+
+
+    // double x1{ std::pow(3.0, 4.0) };
+
+
+	// std::cout << powint(7, 12); // 7 to the 12th power.( Raise 7 to the power of 12th)
+
+    //Quiz 2
+    int x{};
     std::cout << "Enter an integer: ";
-	int x{};
-	std::cin >> x;
+    std::cin >> x;
 
-	std::cout << "Enter another integer: ";
-	int y{};
-	std::cin >> y;
-
-    std::cout << "The remainder is " << x % y << "\n";
-
-    if ((x % y) == 0)
-        std::cout << x << " is evenly divisible by " << y << "\n";
-    else
-        std::cout << x << " is not evenly divisible by " << y << "\n";
+    if (isEven(x) == true) 
+        std::cout << x << " is even " << "\n";
+    else if (isOdd(x) == true)
+        std::cout << x << " is odd " << "\n";
 
 
-    double x1{ std::pow(3.0, 4.0) };
 
 
-	std::cout << powint(7, 12); // 7 to the 12th power.( Raise 7 to the power of 12th)
+
 
 
 }
