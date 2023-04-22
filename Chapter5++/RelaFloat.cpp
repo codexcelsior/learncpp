0e7f775bc7e04a3cf86616cd8101b9dab806da72
codexcelsior <quang13012005@gmail.com>
2023-04-22 12:22:09 +0700

    RelaFloat Done

---
+++ Chapter5++/RelaFloat.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter another integer: ";
+    int y{};
+    std::cin >> y;
+
+    if (x == y)
+        std::cout << x << " equals " << y << '\n';
+    if (x != y)
+        std::cout << x << " does not equal " << y << '\n';
+    if (x > y)
+        std::cout << x << " is greater than " << y << '\n';
+    if (x < y)
+        std::cout << x << " is less than " << y << '\n';
+    if (x >= y)
+        std::cout << x << " is greater than or equal to " << y << '\n';
+    if (x <= y)
+        std::cout << x << " is less than or equal to " << y << '\n';
+        
+    
+    //if (b1) ... don't write (b1 == true)
+    //if (!b1) ... don't write (b1 != true)
+    
+    
+    double d1{ 100.0 - 99.99 }; // should equal 0.01 mathematically
+    double d2{ 10.0 - 9.99 }; // should equal 0.01 mathematically
+
+    if (d1 == d2)
+        std::cout << "d1 == d2" << '\n';
+    else if (d1 > d2)
+        std::cout << "d1 > d2" << '\n';
+    else if (d1 < d2)
+        std::cout << "d1 < d2" << '\n';
+
+    constexpr double gravity { 9.8 };
+    //if (gravity == 9.8) // okay if gravity was initialized with a literal
+    // we're on earth
+    
+    return 0;
+}
