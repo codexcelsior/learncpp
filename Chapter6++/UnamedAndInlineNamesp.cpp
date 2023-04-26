3c517f3cbcb5f55de290f80e8a1bcf48f2a1979a
codexcelsior <quang13012005@gmail.com>
2023-04-26 19:10:51 +0700

    UnamedAndInlineNamespace Done

---
+++ Chapter6++/UnamedAndInlineNamesp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+namespace v1 // declare a normal namespace named v1
+{
+    void doSomething()
+    {
+        std::cout << "v1\n";
+    }
+}
+
+inline namespace v2 // declare an inline namespace named v2
+{
+    void doSomething()
+    {
+        std::cout << "v2\n";
+    }
+}
+
+int main()
+{
+    v1::doSomething(); // calls the v1 version of doSomething()
+    v2::doSomething(); // calls the v2 version of doSomething()
+
+    doSomething(); // calls the inline version of doSomething() (which is v2)
+    //Give some manners to the compiler and it will execute the ones that is declared incline*
+
+    return 0;
+}
