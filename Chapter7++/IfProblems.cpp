b670d411307e0e38efddf70060cc7c9cd1a3c702
codexcelsior <quang13012005@gmail.com>
2023-04-27 18:37:35 +0700

    IfPrblems Done

---
+++ Chapter7++/IfProblems.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+
+
+    if (x < 0)
+        std::cout << x << " is negative\n";
+    else if (x <= 20) // only executes if x >= 0
+        std::cout << x << " is between 0 and 20\n";
+    else // only executes if x > 20
+        std::cout << x << " is greater than 20\n";
+
+
+
+    //Dangling Else
+        if (x >= 0) // outer if statement
+        // it is bad coding style to nest if statements this way
+        if (x <= 20) // inner if statement
+            std::cout << x << " is between 0 and 20\n";
+
+    // which if statement does this else belong to?
+    else
+        std::cout << x << " is negative\n";
+
+
+    //Null statement
+    if (x > 10)
+        ; // this is a NULL statement
+
+
+    //Flattening
+    if (x < 0)
+        std::cout << x << " is negative\n";
+    else if (x <= 20) // only executes if x >= 0
+        std::cout << x << " is between 0 and 20\n";
+    else // only executes if x > 20
+        std::cout << x << " is greater than 20\n";
+
+
+    return 0;
+}
