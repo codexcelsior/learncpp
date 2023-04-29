ed02e5f458d8643d9f1c4a2e34c2ab8661dd68d7
codexcelsior <quang13012005@gmail.com>
2023-04-29 13:21:35 +0700

    Loop And While Done

---
+++ Chapter7++/LoopAndWHile/ex2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main()
+{
+    int outer{1};
+    while (outer <= 5)
+    {
+        int inner{5};
+        while (inner >= 1)
+        {
+            if (inner <= outer)
+            {
+                std::cout << inner << " ";
+            }
+            else 
+            {
+                std::cout << "  ";
+            }
+            inner--;
+        }
+        outer++;
+        std::cout << "\n";
+    }
+
+
+//Second way
+
+for (int outer {1} ; outer <=5; outer++)
+{
+    for (int inner {5}; inner >= 1; inner-- )
+    {
+        if (inner <= outer)
+        {
+            std::cout << inner << " ";
+        }
+        else 
+        {
+            std::cout << "  ";
+        }
+    }
+    std::cout << "\n";
+}
+
+
+
+
+
+
+
+
+
+}
