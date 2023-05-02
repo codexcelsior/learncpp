b573227c26cb0ace5887c02e942957f6d96acf00
codexcelsior <quang13012005@gmail.com>
2023-05-02 23:48:19 +0700

    Q1 Done

---
+++ Chapter7++/Summary/Q1/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "constants.h"
+
+double calculateHeight(double initialHeight, int seconds)
+{
+    double distanceFallen { myConstants::gravity * seconds * seconds / 2 };
+    double heightNow { initialHeight - distanceFallen };
+
+    // Check whether we've gone under the ground
+    // If so, set the height to ground-level
+    if (heightNow < 0.0)
+        return 0.0;
+    else
+        return heightNow;
+}
+
+void calculateAndPrintHeight(double initialHeight, int time)
+{
+    std::cout << "At " << time << " seconds, the ball is at height: " << calculateHeight(initialHeight, time) << '\n';
+}
+
+int main()
+{
+    std::cout << "Enter the initial height of the tower in meters: ";
+    double initialHeight {};
+    std::cin >> initialHeight;
+
+
+    for (int second{0}; ; second++)
+    {
+        calculateAndPrintHeight(initialHeight, second);
+        if (calculateHeight(initialHeight, second) == 0)
+        {
+            break;
+        }
+    }
+
+    int second1{};
+
+    while (calculateHeight( initialHeight, second1) != 0)
+    {
+        calculateAndPrintHeight(initialHeight, second1);
+        second1++;
+    }
+
+    return 0;
+}
