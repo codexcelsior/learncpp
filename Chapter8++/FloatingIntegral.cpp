f97a6051fbd89a7e3137a3a9b7098056eba442a7
codexcelsior <quang13012005@gmail.com>
2023-05-05 00:01:56 +0700

    Floating And Integral Done

--- Chapter8++/FloatingIntegral.cpp
+++ Chapter8++/FloatingIntegral.cpp
@@ -18,5 +18,13 @@ int main()
     printDouble(5.0); // no conversion necessary
     printDouble(4.0f); // numeric promotion of float to double
 
+    printInt(2);
+
+    short s{ 3 }; // there is no short literal suffix, so we'll use a variable for this one
+    printInt(s); // numeric promotion of short to int
+
+    printInt('a'); // numeric promotion of char to int
+    printInt(true); // numeric promotion of bool to int
+
     return 0;
 }
