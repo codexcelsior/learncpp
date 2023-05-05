d62bc32e7eb840e3dc2648bcbac5c9b2e1409efa
codexcelsior <quang13012005@gmail.com>
2023-05-05 23:59:39 +0700

    Type Deduct Funct Done

--- Chapter8++/TypeDeductFunct.cpp
+++ Chapter8++/TypeDeductFunct.cpp
@@ -24,13 +24,13 @@ auto printSomething() -> void;
 auto generateSubstring(const std::string &s, int start, int len) -> std::string;
 
 
-void addAndPrint(auto x, auto y)
-{
-    std::cout << x + y << '\n';
-}
+// void addAndPrint(auto x, auto y)
+// {
+//     std::cout << x + y << '\n';
+// }
 
 int main()
 {
     addAndPrint(2, 3); // case 1: call addAndPrint with int parameters
-    addAndPrint(4.5, 6.7); // case 2: call addAndPrint with double parameters
+    // addAndPrint(4.5, 6.7); // case 2: call addAndPrint with double parameters
 }
