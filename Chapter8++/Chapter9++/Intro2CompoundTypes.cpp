51655b95e2030abb573109fe985774169f5297b6
codexcelsior <quang13012005@gmail.com>
2023-05-09 23:59:35 +0700

    Intro2CT Done

---
+++ Chapter8++/Chapter9++/Intro2CompoundTypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    // Our first fraction
+    int num1 {};
+    int den1 {};
+
+    // Our second fraction
+    int num2 {};
+    int den2 {};
+
+    // Used to eat the slash between the numerator and denominator
+    char ignore {};
+
+    std::cout << "Enter a fraction: ";
+    std::cin >> num1 >> ignore >> den1;
+
+    std::cout << "Enter a fraction: ";
+    std::cin >> num2 >> ignore >> den2;
+
+    std::cout << "The two fractions multiplied: "
+        << num1 * num2 << '/' << den1 * den2 << '\n';
+
+    int id1 { 42 };
+    int id2 { 57 };
+    int id3 { 162 };
+    // and so on
+
+    return 0;
+}
